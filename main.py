c2cc1e8b57b6f75a5527f7956a1afb8344302b41
al-ov73 <klever1987@yandex.ru>
2024-10-31 12:47:06 +0000

    add unicorn

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import asyncio
+import fastapi
 import logging
 import sys
 from os import getenv
+
+from unicorn import Unicorn
 
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
@@ -11,21 +14,19 @@ from aiogram.types import Message
 
 TOKEN = '6914939892:AAHxAsWcHIEIcaEz0qgtLGY6XA0Fz64n95A'
 
-# All handlers should be attached to the Router (or Dispatcher)
-
 dp = Dispatcher()
 
+app = fastapi.FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """
     This handler receives messages with `/start` command
     """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
 
@@ -37,21 +38,25 @@ async def echo_handler(message: Message) -> None:
     By default, message handler will handle all message types (like a text, photo, sticker etc.)
     """
     try:
-        # Send a copy of the received message
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
-        # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
 
 
 async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    # And the run events dispatching
     await dp.start_polling(bot)
 
 
+def run_server():
+    unicorn = Unicorn(worker_class='sync', config={
+        'bind': '127.0.0.1:8000',
+        'worker_processes': 4,
+        'timeout': 60
+    })
+    unicorn.run()
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    run_server()
     asyncio.run(main())
